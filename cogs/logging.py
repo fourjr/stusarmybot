d20d43f41bc01a9fe4fa1f1e8e01dedf37920dba
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-09-26 18:44:24 +0800

    pings

--- cogs/logging.py
+++ cogs/logging.py
@@ -39,7 +39,7 @@ class Logging:
 		embed.add_field(name="Stu's Army 2", value="2800 Trophies", inline=True)
 		embed.add_field(name="Stu's Army 3", value="2400 Trophies", inline=True)
 		embed.add_field(name="Stu's Army 4", value="2000 Trophies", inline=True)
-		welcome = await self.bot.send_message(member.server.get_channel('298816198349553665'), member.mention, embed=embed)
+		welcome = await self.bot.send_message(member.server.get_channel('298816198349553665'), '{} <@&334250664870019073> <@277389105501831170>'.format(member.mention), embed=embed)
 		await self.bot.edit_message(welcome, '\u200B', embed=embed)
 		
 def setup(bot):
