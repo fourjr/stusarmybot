7aeed2c59a04e0552beb6b829a4db5f631537702
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-09-26 18:45:56 +0800

    def setup and channel restrictions

--- cogs/commands.py
+++ cogs/commands.py
@@ -11,6 +11,7 @@ class Logging:
     @commands.command(pass_context=True)
     async def trophy(self, ctx, trophy:int):
         '''We will suggest Clans that meet your trophy level!'''
+        if ctx.message.channel != '298816198349553665' and ctx.message.chaannel != '362172188301852672': return
         if trophy >= 3800:
             await self.bot.say("You can check out Stu's Army 1! <@277389105501831170>, help him out!")
         elif trophy >= 2800:
@@ -21,3 +22,6 @@ class Logging:
 	        await self.bot.say("You can check out Stu's Army 2! <@277389105501831170>, help him out!")
         else:
             await self.bot.say("I'm sorry but you don't meet the criteria for any of our Clans. You can do `>visitor` if you want to stick around though!")
+
+def setup(bot):
+    bot.add_cog(Logging(bot))
