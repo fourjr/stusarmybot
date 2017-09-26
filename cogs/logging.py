9e523b99c574abe8c1e30e275bf72b4d7f3d1898
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-09-26 18:16:58 +0800

    remove autoroles

--- cogs/logging.py
+++ cogs/logging.py
@@ -33,12 +33,7 @@ class Logging:
 		await self.bot.send_message(message.server.get_channel('362175558043566080'), embed=embed)
 		
 	async def on_member_join(self, member):
-		await self.bot.add_roles(member, discord.utils.get(member.server.roles, id='345834741829730305'))
-		await asyncio.sleep(120)
 		tup1 = (0xff0f7f, 0xff0f1a, 0x2ef65c, 0xf5f404, 0x0da1e8, 0xffa200, 0xd96af2)
-		await self.bot.add_roles(member, discord.utils.get(member.server.roles, id='317621138274648066'))
-		await asyncio.sleep(0.5)
-		await self.bot.remove_roles(member, discord.utils.get(member.server.roles, id='345834741829730305'))
 		embed=discord.Embed(title="Hello {}!".format(member.name), description="Welcome To {}! Below listed are our Clans!".format(member.server.name , color = random.choice(tup1[0:6]))
 		embed.add_field(name="Stu's Army 1", value="3800 Trophies", inline=True)
 		embed.add_field(name="Stu's Army 2", value="2800 Trophies", inline=True)
