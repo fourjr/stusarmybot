26670f34b28acf3c95893ac774cf9c23f72189d4
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-09-27 13:01:03 +0800

    >visitor

--- cogs/commands.py
+++ cogs/commands.py
@@ -11,7 +11,9 @@ class Commands:
     @commands.command(pass_context=True)
     async def visitor(self, ctx):
         '''Get the Visitor and Member Role!'''
-        await self.bot.add_roles(member, discord.utils.get(ctx.message.server.roles, id='298817009372889088'), discord.utils.get(ctx.message.server.roles, id='298815975980138496'))
+        if ctx.message.channel.id != '298816198349553665' and ctx.message.channel.id != '362172188301852672': return
+        await self.bot.add_roles(ctx.message.author, discord.utils.get(ctx.message.server.roles, id='298817009372889088'), discord.utils.get(ctx.message.server.roles, id='298815975980138496'))
+        await self.bot.say('I have given {} the Visitor and Member Roles!'.format(ctx.message.author.name))
     
     @commands.command(pass_context=True)
     async def trophy(self, ctx, trophy:int):
@@ -31,28 +33,28 @@ class Commands:
     @commands.command(pass_context=True, aliases=['SA1'])
     @commands.has_role("Welcome Assistant") 
     async def sa1(self, ctx, member:discord.Member):
-        '''Gives the SA1 role and Member role to the specified Member'''
+        '''Gives the SA1 role and Member role to the specified member'''
         await self.bot.add_roles(member, discord.utils.get(ctx.message.server.roles, id='298817009372889088'), discord.utils.get(ctx.message.server.roles, id='298816849968234496'))
         await self.bot.say('Given the SA1 Roles!')
             
     @commands.command(pass_context=True, aliases=['SA2'])
     @commands.has_role("Welcome Assistant") 
     async def sa2(self, ctx, member:discord.Member):
-        '''Gives the SA2 role and Member role to the specified Member'''
+        '''Gives the SA2 role and Member role to the specified member'''
         await self.bot.add_roles(member, discord.utils.get(ctx.message.server.roles, id='298817009372889088'), discord.utils.get(ctx.message.server.roles, id='298816905504882698'))
         await self.bot.say('Given the SA2 Roles!')
             
     @commands.command(pass_context=True, aliases=['SA3'])
     @commands.has_role("Welcome Assistant") 
     async def sa3(self, ctx, member:discord.Member):
-        '''Gives the SA3 role and Member role to the specified Member'''
+        '''Gives the SA3 role and Member role to the specified member'''
         await self.bot.add_roles(member, discord.utils.get(ctx.message.server.roles, id='298817009372889088'), discord.utils.get(ctx.message.server.roles, id='299912276008501248'))
         await self.bot.say('Given the SA3 Roles!')
    
     @commands.command(pass_context=True, aliases=['SA4'])
     @commands.has_role("Welcome Assistant") 
     async def sa4(self, ctx, member:discord.Member):
-        '''Gives the SA4 role and Member role to the specified Member'''
+        '''Gives the SA4 role and Member role to the specified member'''
         await self.bot.add_roles(member, discord.utils.get(ctx.message.server.roles, id='298817009372889088'), discord.utils.get(ctx.message.server.roles, id='329922314747641859'))
         await self.bot.say('Given the SA4 Roles!')
         
