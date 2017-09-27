b491beedaf1ab2d0d58fdb1e72366bddea73ff09
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-09-27 11:46:04 +0800

    Create checks.py

---
+++ ext/checks.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+import discord.utils
+
+def is_owner_check(message):
+    return message.author.id == '180314310298304512'
+  
+
+def role_or_permissions(ctx, check, **perms):
+    if check_permissions(ctx, perms):
+        return True
+
+    ch = ctx.message.channel
+    author = ctx.message.author
+    if ch.is_private:
+        return False # can't have roles in PMs
+
+    role = discord.utils.find(check, author.roles)
+    return role is not None
+
+def mod_or_permissions(**perms):
+    def predicate(ctx):
+        return role_or_permissions(ctx, lambda r: r.name in ('Welcome Assistant'), **perms)
+
+return commands.check(predicate)
