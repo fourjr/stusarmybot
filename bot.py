373a99b8095fe53010baee550cf2ebd1ed06cca7
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-10-08 20:34:26 +0800

    ndent

--- bot.py
+++ bot.py
@@ -37,16 +37,17 @@ async def ping(ctx):
 @bot.command()
 async def help(ctx):
     'Shows.. help?'
-    await ctx.send("""This isn't kept up to date 100% because I'm lazy :)
-    **Welcome Channel**
-    `>sa1` to `>sa5` - Give appropriate roles
-    `>visitor` - Give appropriate roles
-    
-    **Non-Welcome Channel**
-    `>help` - Shows this ~~never updated~~ list of useful information.
-    `>ping` - Pong!
-    `>update` - Updates <#365870449915330560>
-    `>sa1info` to `>sa4info` - Gives you Clan Stats for the various Clans. """)
+    await ctx.send("""
+This isn't kept up to date 100% because I'm lazy :)
+**Welcome Channel**
+`>sa1` to `>sa5` - Give appropriate roles
+`>visitor` - Give appropriate roles
+
+**Non-Welcome Channel**
+`>help` - Shows this ~~never updated~~ list of useful information.
+`>ping` - Pong!
+`>update` - Updates <#365870449915330560>
+`>sa1info` to `>sa4info` - Gives you Clan Stats for the various Clans. """)
 
 @bot.command()
 async def restart(ctx):
