b6424b894772d603b1bf02a1bfd5bc5377f605f4
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-10-27 22:09:04 +0800

    change it to >clan sa1

--- cogs/stats.py
+++ cogs/stats.py
@@ -171,6 +171,42 @@ class Stats():
         '''Shows some Clan Statistics'''
         async with ctx.channel.typing():
             emoji = self.bot.emoji
+            if clantag.lower() == 'sa1' or clantag.lower() == 'sa2' or clantag.lower() == 'sa3' or clantag.lower() == 'sa4' or clantag.lower() == 'sa5':
+                if clantag.lower() == 'sa1': tag = '88PYQV'
+                if clantag.lower() == 'sa1': tag = '29UQQ282'
+                if clantag.lower() == 'sa1': tag = '28JU8P0Y'
+                if clantag.lower() == 'sa1': tag = '8PUUGRYG'
+                if clantag.lower() == 'sa1': tag = '8YUU2CQV'
+                async with aiohttp.ClientSession().get(f'http://api.cr-api.com/clan/{tag}') as d:
+                    data = await d.json()
+
+                em = discord.Embed(color=discord.Color(value=3407664), title=f'''{data['name']} (#{tag})''', description=f'''{data['description']}''')
+                em.set_author(name='Clan', url=f'''http://cr-api.com/clan/{tag}''', icon_url=f'''http://api.cr-api.com{data['badge']['url']}''')
+                em.set_thumbnail(url=f'''http://api.cr-api.com{data['badge']['url']}''')
+                em.add_field(name='Trophies', value=str(data['score']), inline=True)
+                em.add_field(name='Type', value=data['typeName'], inline=True)
+                em.add_field(name='Member Count', value=f'''{data['memberCount']}/50''', inline=True)
+                em.add_field(name='Requirement', value=str(data['requiredScore']), inline=True)
+                em.add_field(name='Donations', value=str(data['donations']), inline=True)
+                em.add_field(name='Region', value=data['region']['name'])
+                players = []
+                for i in range(len(data['members'])):
+                    if (i <= 2):
+                        players.append(f'''{data['members'][i]['name']}: {data['members'][i]['trophies']}
+        (#{data['members'][i]['tag']})''')
+                em.add_field(name='Top 3 Players', value='\n\n'.join(players), inline=True)
+                contributors = sorted(data['members'], key=(lambda x: x['clanChestCrowns']))
+                contributors = list(reversed(contributors))
+                players = []
+                for i in range(len(data['members'])):
+                    if (i <= 2):
+                        players.append(f'''{contributors[i]['name']}: {contributors[i]['clanChestCrowns']}
+        (#{contributors[i]['tag']})''')
+                em.add_field(name='Top CC Contributors', value='\n\n'.join(players), inline=True)
+                em.set_footer(text='Powered by cr-api', icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
+                await ctx.send(embed=em)
+                return
+
             db = False
             tag = None
             if len(ctx.message.raw_mentions) == 1:
