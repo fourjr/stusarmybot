123d336d6ca6abf9ece190e6cf369ea2c40e88ff
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-11-13 16:07:30 +0800

    Typo

--- cogs/mod.py
+++ cogs/mod.py
@@ -21,7 +21,7 @@ class Mod():
         except:
             await ctx.send("I can't kick this user!")
         else:
-            await ctx.send(f'{ctx.author.name} kicked {member.name}')
+            await ctx.send(f'{ctx.author.name} banned {member.name}')
 
 def setup(bot):
     bot.add_cog(Mod(bot))
