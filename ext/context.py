0406377955bc9c33a4a9fdaca4a27ba24023f031
fourjr <28086837+fourjr@users.noreply.github.com>
2018-04-28 09:42:30 +0000

    hopefully this works

--- ext/context.py
+++ ext/context.py
@@ -10,3 +10,28 @@ class CustomContext(commands.Context):
     @property
     def session(self):
         return self.bot.session
+
+    @staticmethod
+    def paginate(text: str):
+        '''Simple generator that paginates text.'''
+
+        last = 0
+
+        pages = []
+
+        for curr in range(0, len(text)):
+
+            if curr % 1980 == 0:
+
+                pages.append(text[last:curr])
+
+                last = curr
+
+                appd_index = curr
+
+        if appd_index != len(text)-1:
+
+            pages.append(text[last:curr])
+
+        return list(filter(lambda a: a != '', pages))
+
