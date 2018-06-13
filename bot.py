7f0277c9041a02939860fab9d80efe8ccdcdd495
fourjr <28086837+fourjr@users.noreply.github.com>
2018-06-13 17:26:23 +0800

    fix oncmderror

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ class Bot(commands.Bot):
         print('Ready')
 
 
-    async def on_command_error(ctx, error):
+    async def on_command_error(self, ctx, error):
         trace = ''.join(traceback.format_exception(type(error), error, error.__traceback__))
         erroremb = discord.Embed(description=f'```py\n{trace}\n```', color=discord.Color.red(), timestamp=ctx.message.created_at)
         erroremb.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
@@ -83,6 +83,7 @@ class Bot(commands.Bot):
         erroremb.add_field(name='Location', value=f'#{ctx.channel.name} ({ctx.channel.id})')
 
         await self.get_channel(375113574038896640).send(embed=erroremb)
+        print(trace)
 
         if isinstance(error, InvalidTag):
             await ctx.send(error.message)
